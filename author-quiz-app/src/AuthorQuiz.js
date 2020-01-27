2c4c029e1ab68d1ca554b7dac7e673d461d70389
viswajeetk <viswajeetk@INviswajeetkumars-MacBook-Pro.local>
2020-01-27 12:06:02 +0530

    Wrote few tests

--- author-quiz-app/src/AuthorQuiz.js
+++ author-quiz-app/src/AuthorQuiz.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './AuthorQuiz.css';
 import './bootstrap.min.css';
 
@@ -11,15 +12,37 @@ function Hero() {
   </div>);
 }
 
-function Turn({author, books}) {
-  return (<div className="row turn" style={{backgroundColor: "white"}}>
+function Turn({author, books, highlight, onAnswerSelected}) {
+  function highlightToBgColor(highlight) {
+    const mapping = {
+      'none': '',
+      'correct': 'green',
+      'wrong': 'red'
+    };
+    return mapping[highlight];
+  }
+
+ 
+  return (<div className="row turn" style={{backgroundColor: highlightToBgColor(highlight)}}>
     <div className="col-4 offset-1">
       <img src={author.imageUrl} className="authorimage" alt="Author" />
     </div>
     <div className="col-6">
-      {books.map((title) => <Book title={title} key={title} />)}
+      {books.map((title) => <Book title={title} key={title} onClick={onAnswerSelected} />)}
     </div>
   </div>);
+}
+
+Turn.propTypes = {
+  author: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    imageSource: PropTypes.string.isRequired,
+    books: PropTypes.arrayOf(PropTypes.string).isRequired
+  }),
+  books: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onAnswerSelected: PropTypes.func.isRequired,
+  highlight: PropTypes.string.isRequired
 }
 
 function Continue() {
@@ -36,17 +59,17 @@ function Footer() {
   </div>);
 }
 
-function Book({title}) {
-  return (<div className="answer">
+function Book({title, onClick}) {
+  return (<div className="answer" onClick={() => {onClick(title);}}>
     <h4>{title}</h4>
   </div>);
 }
 
-function AuthorQuiz({turnData}) {
+function AuthorQuiz({turnData, highlight, onAnswerSelected}) {
   return (
     <div className="container-fluid">
       <Hero />
-      <Turn {...turnData}/>
+      <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected} />
       <Continue />
       <Footer />
     </div>
--- author-quiz-app/src/AuthorQuiz.test.js
+++ author-quiz-app/src/AuthorQuiz.test.js
@@ -1,9 +1,78 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import ReactDOM from 'react-dom';
+import AuthorQuiz from './AuthorQuiz';
+import Enzyme, {mount, shallow, render} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const state = {
+  turnData: {
+    books: ['The Shining', 'IT', 'David Copperfield', 'A Tale of Two Cities', 'Hamlet'],
+    author: {
+      name: 'Charles Dickens',
+      imageUrl: 'images/authors/charlesdickens.jpg',
+      imageSource: 'Wikimedia Commons',
+      books: ['David Copperfield', 'A Tale of Two Cities']
+    },
+  },
+  highlight: 'none'
+};
+
+describe("Author Quiz", () => {
+  it("renders without crashing", () => {
+      const div = document.createElement("div");
+      ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={() => {}} />, div);
+  });
+
+  describe("When no answer has been selected", () => {
+    let wrapper;
+    beforeAll(() => {
+      wrapper = mount(<AuthorQuiz {...state} onAnswerSelected={() => {}} />);
+    });
+
+    it("should have no background color", () => {
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('');
+    });
+  });
+
+  describe("When a wrong answer has been selected", () => {
+    let wrapper;
+    beforeAll(() => {
+      wrapper = mount(<AuthorQuiz {...(Object.assign({}, state, {highlight: 'wrong'}))} onAnswerSelected={() => {}} />)
+    });
+
+    it('should have a red background color', () => {
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('red');
+    });
+  });
+
+  describe("When a correct answer has been selected", () => {
+    let wrapper;
+
+    beforeAll(() => {
+      wrapper = mount(<AuthorQuiz {...(Object.assign({}, state, {highlight: 'correct'}))} onAnswerSelected={() => {}} />);
+    });
+
+    it('should have a green background color', () => {
+      expect(wrapper.find("div.row.turn").props().style.backgroundColor).toBe('green');
+    });
+  });
+
+  describe("When the first answer is selected", () => {
+    let handleAnswerSelection = jest.fn();
+
+    let wrapper;
+    beforeAll(() => {
+      wrapper = mount(<AuthorQuiz {...state} onAnswerSelected={handleAnswerSelection} />);
+      wrapper.find('.answer').first().simulate('click');
+    });
+
+    it('onAnswerSelected should be called', () => {
+      expect(handleAnswerSelection).toHaveBeenCalled();
+    });
+
+    it('should receive The Shining', () => {
+      expect(handleAnswerSelection).toHaveBeenCalledWith('The Shining');
+    });
+  });
 });
