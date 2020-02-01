e34cd96aeb3480a47f24b3b9fcecc72c005675cf
viswajeetk <viswajeetk@INviswajeetkumars-MacBook-Pro.local>
2020-02-02 01:27:52 +0530

    Updates

---
+++ precache-manifest.7b7401535c009d7b91635f6ceab6c612.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d74b8b3c3c9ecfa523b23b2be270f2ad",
+    "url": "/author-quiz-app/index.html"
+  },
+  {
+    "revision": "f1f6c204706a21e31d80",
+    "url": "/author-quiz-app/static/css/main.43824766.chunk.css"
+  },
+  {
+    "revision": "70e78ee2b7ab0d21a8e3",
+    "url": "/author-quiz-app/static/js/2.af8e084c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/author-quiz-app/static/js/2.af8e084c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f1f6c204706a21e31d80",
+    "url": "/author-quiz-app/static/js/main.3651cbfc.chunk.js"
+  },
+  {
+    "revision": "f2f064ac37a2cfa8c08e",
+    "url": "/author-quiz-app/static/js/runtime-main.1c004432.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/AuthorQuizReact/precache-manifest.5056fe9f1afc2f1750c5fbb5d4db0133.js"
+  "/author-quiz-app/precache-manifest.7b7401535c009d7b91635f6ceab6c612.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AuthorQuizReact/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/author-quiz-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
